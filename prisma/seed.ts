6cd7e3dec7b1d09115f2ae7dda83f307777a1932
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-06-21 00:22:24 +0300

    added prisma database

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  const user_1 = prisma.user.upsert({
+    where: { email: 'omar@admin.com' },
+    update: {},
+    create: {
+      email: 'omar@admin.com',
+      password: '1234',
+    },
+  });
+
+  console.log({ user_1 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
